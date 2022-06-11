e75945aee1cd27de616524fcbeaff6a9c6d2d233
小泉大夢 <koizumitaimu@MacBook-Air.local>
2022-06-11 19:44:55 +0900

    add google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,15 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Noto+Sans+JP:wght@100;300;400;500;700;900&display=swap" rel="stylesheet" />
+          {/* Global site tag (gtag.js) - Google Analytics */}
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-W08ZWCM33K"></script>
+          <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-W08ZWCM33K');
+          </script>
         </Head>
         <body>
           <Main />
